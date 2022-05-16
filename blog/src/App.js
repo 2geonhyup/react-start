a631d8ed95554ee88c2c061f596ffc0da34ba98f
이건협 <gh@igeonhyeob-ui-MacBookAir.local>
2022-05-16 15:28:01 +0900

    y

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,23 +3,23 @@ import './App.css';
 import {BlackNav} from 'react-bootstrap';
 import {useState} from "react";
 
+
+
 function App() {
   let [post, changePost] = useState(["남자코트 추천", "강남 우동맛집", "파이썬독학"]);
-let originPost = ["남자코트 추천", "강남 우동맛집", "파이썬독학"];
-let secandPost = ["여자코트 추천", "강북 우동맛집", "자바 독학"];
 
-  function onClick (i) {
+  const postMap = 
+    {
+      "남자코트 추천":"여자코트 추천","강남 우동맛집":"전북 우동맛집", "파이썬독학":"자바독학",
+      "여자코트 추천":"남자코트 추천","전북 우동맛집":"강남 우동맛집", "자바독학":"파이썬독학"
+};
+
+  function onClick (text) {
     let copy = [...post];
-    if(post[i] !== originPost[i]){
-      
-      copy[i] = secandPost[i];
-      
-    }else {
-      copy[i] = originPost[i];
-    }
-    copy.sort();
-    
-    changePost(copy);
+    let newCopy = copy.map(function(val) { console.log(val, text);
+      if(val===text) {console.log(postMap[text]);return postMap[text]}else{return val}});
+      newCopy.sort();
+    changePost(newCopy);
   }
 
 
@@ -33,17 +33,17 @@ let secandPost = ["여자코트 추천", "강북 우동맛집", "자바 독학"]
       <div className='list'>
         <h4>{post[0]}</h4>
         <p>2월 17일 발행</p>
-        <button onClick={()=>onClick(0)}>변경</button>
+        <button onClick={()=>onClick(post[0])}>변경</button>
       </div>
       <div className='list'>
         <h4>{post[1]}</h4>
         <p>2월 17일 발행</p>
-        <button onClick={()=>onClick(1)}>변경</button>
+        <button onClick={()=>onClick(post[1])}>변경</button>
       </div>
       <div className='list'>
         <h4>{post[2]}</h4>
         <p>2월 17일 발행</p>
-        <button onClick={()=>onClick(2)}>변경</button>
+        <button onClick={()=>onClick(post[2])}>변경</button>
       </div>
     </div>
   )
